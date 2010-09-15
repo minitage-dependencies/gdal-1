10466dad7458b275988346f7a22482e75e159eb9
kiorky <kiorky@cryptelium.net>
2010-09-15 06:13:42 +0000

    up

--- hooks/gdal.py
+++ hooks/gdal.py
@@ -4,4 +4,10 @@ def getgdalenv(options,buildout):
     # see http://trac.osgeo.org/gdal/wiki/FAQInstallationAndBuilding#HowcaniaddparticularLDFLAGSwithGDAL1.5 (little hack (kiorky))
     os.environ['LNK_FLAGS'] = os.environ['LDFLAGS']
     os.environ['LIBS'] = os.environ['LDFLAGS']
+def installswq(options,buildout):
+    # copy in LNK_FLAGS for gdal < 1.5 !
+    # see http://trac.osgeo.org/gdal/wiki/FAQInstallationAndBuilding#HowcaniaddparticularLDFLAGSwithGDAL1.5 (little hack (kiorky))
+    swqh = os.path.join(buildout['buildout']['directory'], 'parts', 'part', 'include', 'swq.h')
+    swqho = os.path.join(buildout['buildout']['directory'], 'swq.h')
+    open(swqh , "w").write(open(swqho).read())
 # vim:set ts=4 sts=4 et  :
